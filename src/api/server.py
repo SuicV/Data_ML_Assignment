0f0af136b2063f136adaf04e4f6a32ecab2b6e3e
SuicV <drissoulhbibcontact@gmail.com>
2023-10-10 22:10:41 +0100

    add database connection

--- src/api/server.py
+++ src/api/server.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 
-from src.api.inference_route import inference_router
+from src.api.routes.inference import inference_router
 from src.api.constants import APP_NAME, API_PREFIX
+from src.db import models
+from src.db.sqlite_connector import engine
 
 def server() -> FastAPI:
+    # create db with tables
+    models.Base.metadata.create_all(bind=engine)
+
     app = FastAPI(
         title=APP_NAME,
         docs_url=f"{API_PREFIX}/docs",

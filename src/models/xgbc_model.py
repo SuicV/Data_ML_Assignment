9f1ad490b1d1239f0a3848184efa5a55ddca7afa
SuicV <drissoulhbibcontact@gmail.com>
2023-10-11 15:24:07 +0100

    add stop_words and min_df params

--- src/models/xgbc_model.py
+++ src/models/xgbc_model.py
@@ -10,8 +10,8 @@ class XGBCModel(BaseModel):
     def __init__(self, **kwargs):
         super().__init__(
             model=Pipeline([
-                #('countv', CountVectorizer()),
-                ('tfidfv', TfidfVectorizer()),
+                ('countv', CountVectorizer(stop_words='english', min_df=10)),
+                #('tfidfv', TfidfVectorizer(stop_words='english', min_df=30)),
                 ('xgbc', xgb.XGBClassifier(**kwargs))])
         )
         self.name = "XGBoost"
